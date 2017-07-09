837513cfcbb5d2eab841c1ea873e8726163f1f24
Valeriano A.R <valen.alfonso@gmail.com>
2017-07-09 11:15:02 +0200

    Basic tests

---
+++ UrlCompressor.Tests/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace UrlCompressor.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TestUrl("http://google.com");
+            TestUrl("https://google.com");
+            TestUrl("http://facebook.com");
+            TestUrl("https://facebook.com");
+            TestUrl("https://twitter.com");
+            TestUrl("https://twitter.com/Kableado");
+            TestUrl("https://github.com/Kableado");
+
+            Console.Read();
+        }
+
+        private static bool TestUrl(string url)
+        {
+            Console.WriteLine("---------------------------------------------");
+            Console.WriteLine("            Url: {0}", url);
+            string compressedUrl = VAR.UrlCompressor.UrlCompressor.Compress(url);
+            Console.WriteLine("  CompressedUrl: {0}", compressedUrl);
+            string decompressedUrl = VAR.UrlCompressor.UrlCompressor.Decompress(compressedUrl);
+            Console.WriteLine("DecompressedUrl: {0}", decompressedUrl);
+            if(url!= decompressedUrl)
+            {
+                Console.WriteLine("!!!!! Failed !!!!!");
+                return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ VAR.UrlCompressor/UrlCompressor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VAR.UrlCompressor
+{
+    public class UrlCompressor
+    {
+        public static string Compress(string url)
+        {
+            // FIXME: Implement this
+            return string.Empty;
+        }
+
+        public static string Decompress(string compressedUrl)
+        {
+            // FIXME: Implement this
+            return string.Empty;
+        }
+    }
+}
